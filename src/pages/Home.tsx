8db72c6139730982b69c2d728071919d37bd6436
gyo82v <gyo82v@gmail.com>
2026-03-10 20:10:06 +0100

    feat: update the home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,10 @@
+import { useLanguage } from "../i18n/useLanguage";
+
 export default function Home(){
+    const { t } = useLanguage();
     return(
         <section>
-            <p>home here</p>
+            <p>{t("pages.home.title")}</p>
         </section>
     )
 }
