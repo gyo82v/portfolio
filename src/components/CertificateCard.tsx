cbaffc58ce0594ea124473f4bd09c6d4f6114f7a
gyo82v <gyo82v@gmail.com>
2026-03-20 19:25:41 +0100

    feat: update the layout of the certificate card component

--- src/components/CertificateCard.tsx
+++ src/components/CertificateCard.tsx
@@ -14,27 +14,29 @@ export default function CertificateCard({ certificate, onView }: Props) {
   return (
     <article
       className={`
-        flex items-center gap-2 p-3 rounded-2xl
+        flex items-center gap-2 md:gap-3 p-3 md:p-4 rounded-2xl 
         bg-slate-100 dark:bg-slate-800 text-slate-700 dark:text-slate-200
         overflow-hidden ring-1 ring-slate-200 dark:ring-slate-700
       `}
       aria-labelledby={`cert-${certificate.id}-title`}
     >
-      <div className="mt-3 flex-1">
-        <h3 id={`cert-${certificate.id}-title`} className="text-sm font-semibold text-slate-900 dark:text-slate-100">
+      <div className="flex-1 flex flex-col items-center justify-center  md:flex-2 ">
+        <h3 id={`cert-${certificate.id}-title`} className="text-sm text-center font-semibold text-slate-900 dark:text-slate-100">
           {certificate.title}
         </h3>
       </div>
-      <div className="text-xs text-slate-600 dark:text-slate-300 mt-1">
-        <p>{certificate.issuer}<span> · </span>{certificate.date}</p>
+      <div className="text-xs flex-1 flex flex-col items-center justify-center  text-slate-600 dark:text-slate-300 ">
+        <p>{certificate.issuer}</p>
+        <p>{certificate.date}</p>
       </div>
-      <div className="mt-4 flex gap-2">
+      <div className="flex flex-1 md:flex-2 items-center w-full justify-center gap-2 ">
         <button
           type="button"
           onClick={() => onView(certificate)}
-          className={`
-            inline-flex items-center gap-2 px-3 py-2 rounded-md text-sm bg-[var(--accent)] text-white
-            ${transitions} ${focusEffects}
+          className={` w-full
+            inline-flex items-center justify-center gap-1
+            px-3 py-2 rounded-md text-sm bg-[var(--accent)] text-white
+            ${transitions} ${focusEffects} hover:scale-105 active:scale-95
           `}
           aria-label={t("resume.viewCertificate")}
         >
