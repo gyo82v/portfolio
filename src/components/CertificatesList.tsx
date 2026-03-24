6da86d38052d06c2f214c46ed417bd720ec94df7
gyo82v <gyo82v@gmail.com>
2026-03-24 19:50:36 +0100

    feat: add extra prop for the className

--- src/components/CertificatesList.tsx
+++ src/components/CertificatesList.tsx
@@ -3,14 +3,20 @@ import { useState } from "react";
 import type { Certificate } from "../types/dataTypes";
 import CertificateCard from "./CertificateCard";
 
-export default function CertificatesList({ certificates }: {certificates : Certificate[]}) {
+type Props = {
+  certificates: Certificate[]
+  className?: string
+  listClassName?: string
+}
+
+export default function CertificatesList({ certificates, className, listClassName }: Props) {
   const [active, setActive] = useState<Certificate | null>(null);
 
   return (
     <div >
-      <div className="flex flex-col gap-4">
+      <div className={`flex flex-col gap-4 ${listClassName}`}>
         {certificates.map((c) => (
-          <CertificateCard key={c.id} certificate={c} onView={(cert) => setActive(cert)} />
+          <CertificateCard key={c.id} certificate={c} onView={(cert) => setActive(cert)} className={className} />
         ))}
       </div>
       <CertificateModal certificate={active} onClose={() => setActive(null)} />
